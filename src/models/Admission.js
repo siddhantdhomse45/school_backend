eb14c2d686f0f3e0a78aafdfdc4d76263a20276c
siddhantdhomse45 <siddh654@gmail.com>
2026-02-27 23:16:39 +0530

    changes

--- src/models/Admission.js
+++ src/models/Admission.js
@@ -1,3 +1,41 @@
+// import mongoose from "mongoose";
+
+// const admissionSchema = new mongoose.Schema(
+//   {
+//     studentName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     gender: {
+//       type: String,
+//       enum: ["Male", "Female", "Other"],
+//     },
+//     dob: {
+//       type: Date,
+//     },
+//     classApplying: {
+//       type: String,
+//       required: true,
+//     },
+//     fatherName: String,
+//     motherName: String,
+//     mobile: {
+//       type: String,
+//       required: true,
+//     },
+//     email: String,
+//     address: String,
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model("Admission", admissionSchema);
+
+
+
+
+
 import mongoose from "mongoose";
 
 const admissionSchema = new mongoose.Schema(
@@ -7,25 +45,26 @@ const admissionSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    gender: {
-      type: String,
-      enum: ["Male", "Female", "Other"],
-    },
-    dob: {
-      type: Date,
-    },
-    classApplying: {
-      type: String,
-      required: true,
-    },
-    fatherName: String,
-    motherName: String,
+
     mobile: {
       type: String,
       required: true,
     },
-    email: String,
-    address: String,
+
+    classApplying: {
+      type: String,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      required: true,
+    },
+
+    dob: {
+      type: Date,
+      required: true,
+    },
   },
   { timestamps: true }
 );
