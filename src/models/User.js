a35b8155a5bc4ca6bb42345e77115de7b307b2aa
siddhantdhomse45 <siddh654@gmail.com>
2026-03-10 10:12:32 +0530

    changes done

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,55 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//     },
+
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+
+//     role: {
+//       type: String,
+//       enum: ["student", "parent", "staff", "admin"],
+//       required: true,
+//     },
+
+//     contact: {
+//       type: String,
+//       required: function () {
+//         return this.role !== "student";
+//       },
+//       default: undefined,
+//     },
+
+//     // rollNumber: {
+//     //   type: String,
+//     //   unique: true,
+//     //   sparse: true, // ✅ allows multiple null values
+//     //   default: undefined,
+//     // },
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model("User", userSchema);
+
+
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -32,13 +84,6 @@ const userSchema = new mongoose.Schema(
       required: function () {
         return this.role !== "student";
       },
-      default: undefined,
-    },
-
-    rollNumber: {
-      type: String,
-      unique: true,
-      sparse: true, // ✅ allows multiple null values
       default: undefined,
     },
   },
